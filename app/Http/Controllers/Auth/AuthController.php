de97c274d6d3dfdc85ca5cf3f35419311c221f88
Paul Guzman <paul_95pool@hotmail.com>
2017-11-20 13:28:34 -0500

    Se implemento el cambio de estado para evitar el doble logeo

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use App\User;
+use Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Http\Request;
+use Session;
+use Illuminate\Support\Facades\Auth;
+use DB;
+
+
+class AuthController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Registration & Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users, as well as the
+    | authentication of existing users. By default, this controller uses
+    | a simple trait to add these behaviors. Why don't you explore it?
+    |
+    */
+
+    use ThrottlesLogins;
+
+
+    /**
+     * Create a new authentication controller instance.
+     *
+     * @return void
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+        $this->middleware('guest', ['except' => 'getLogout']);
+      
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+   
+
+
+//login
+
+       protected function getLogin()
+    {
+        return view("auth/login");
+    }
+
+
+       
+
+public function postLogin(Request $request)
+   {
+//usuario=User::findOrFail(Auth::user()->id);
+  // $usuario->estado; 
+    $this->validate($request, [
+        'email' => 'required',
+        'password' => 'required',
+    ]);
+
+$credentials = $request->only('email', 'password');
+
+if ($this->auth->attempt($credentials, $request->has('remember')))
+    {
+        $usuarios=User::findOrFail(Auth::user()->id);
+
+      if($usuarios->estado!=1)
+      {
+        DB::select('CALL Estado(?)',array(Auth::user()->id));
+        return redirect('home');
+      }
+      else
+      {
+        $this->auth->logout();
+        Session::flush();
+         return view('error');
+      }
+    // session::flash('msg', 'Thanks for voting');
+        
+    }
+         return view('auth/login');
+
+   // return view('login')->with("msjerror","credenciales incorrectas");
+
+
+    }
+
+
+//login
+
+ //registro   
+
+
+        protected function getRegister()
+    {
+        return view("welcome");
+    }
+
+
+        
+
+    protected function postRegister(Request $request)
+   {
+    $this->validate($request, [
+        'name' => 'required',
+        //'last_name' => 'required',
+        'email' => 'required',
+        'password' => 'required, num',
+    ]);
+
+
+    $data = $request;
+
+
+    $user=new User;
+    $user->name=$data['name'];
+    //$user->last_name=$data['last_name'];
+    $user->email=$data['email'];
+    $user->password=bcrypt($data['password']);
+    $user->estado=false;
+
+    if($user->save()){
+
+         return view("/login");
+               
+    }
+   
+
+   
+
+}
+
+//registro
+
+protected function getLogout()
+    {
+       DB::select('CALL Estado(?)',array(Auth::user()->id));
+        $this->auth->logout();
+        Session::flush();
+
+        return redirect('/');
+    }
+
+public function contacto(){
+        return view('scandir(directory)contacto');
+   }
+
+
+
+
+
+}
---
+++ app/Notifications/MyResetPassword.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Auth\Notifications\ResetPassword;
+
+class MyResetPassword extends ResetPassword
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+        ->subject('Recuperar contraseña')
+        ->greeting('Hola')
+        ->line('Estás recibiendo este correo porque hiciste una solicitud de recuperación de contraseña para tu cuenta.')
+        ->action('Recuperar contraseña', route('password.reset', $this->token))
+        ->line('Si no realizaste esta solicitud, no se requiere realizar ninguna otra acción.')
+        ->line('Nueva contraseña: '. $this->generaPass())
+        ->salutation('Saludos, '. config('app.name'));
+                    /*->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');*/
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+
+
+//GENERADOR DE CONTRASEÑA ALEATORIA
+    public function generaPass(){
+        //Se define una cadena de caractares. Te recomiendo que uses esta.
+        $cadena = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+        //Obtenemos la longitud de la cadena de caracteres
+        $longitudCadena=strlen($cadena);
+         
+        //Se define la variable que va a contener la contraseña
+        $pass = "";
+        //Se define la longitud de la contraseña, en mi caso 10, pero puedes poner la longitud que quieras
+        $longitudPass=10;
+         
+        //Creamos la contraseña
+        for($i=1 ; $i<=$longitudPass ; $i++){
+            //Definimos numero aleatorio entre 0 y la longitud de la cadena de caracteres-1
+            $pos=rand(0,$longitudCadena-1);
+         
+            //Vamos formando la contraseña en cada iteraccion del bucle, añadiendo a la cadena $pass la letra correspondiente a la posicion $pos en la cadena de caracteres definida.
+            $pass .= substr($cadena,$pos,1);
+        }
+        return $pass;
+    }
+}
